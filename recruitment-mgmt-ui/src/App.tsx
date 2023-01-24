2f7cd59dbe6780ced37fe1b4ad581a05525f784e
RISHIK SHRIVASTAV <rishikshrivastav0@gmail.com>
2023-01-24 15:06:25 +0530

    navbar chng

---
+++ recruitment-mgmt-ui/src/App.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar/navbar.component';
+// import { BrowserRouter, Route } from 'react-router-dom'
+// import Counter from './components/Counter/Counter';
+// import { useAppDispatch, useAppSelector } from './redux/hooks';
+// import { counterActions } from './redux/counter/slice';
+import Dashboard from './pages/Dashboard/dashboard.component';
+const App:React.FunctionComponent= ()=> {
+  // const dispatch = useAppDispatch();
+
+  // const { value } = useAppSelector((state) => state.counter);
+
+  // const increment = (): void => {
+  //   dispatch(counterActions.increment());
+  // };
+
+  // const decrement = (): void => {
+  //   dispatch(counterActions.decrement());
+  // };
+
+  // const incrementAsync = (): void => {
+  //   dispatch(counterActions.incrementAsync());
+  // };
+
+  // const decrementAsync = (): void => {
+  //   dispatch(counterActions.decrementAsync());
+  // };
+
+  return (
+   
+    <div className="App">
+
+       <Navbar/>
+    <BrowserRouter>
+       <Routes>
+      <Route path='/' element={<Dashboard/>} />
+      </Routes>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ recruitment-mgmt-ui/src/pages/Dashboard/dashboard.component.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard:React.FunctionComponent = () => {
+  return (
+    <div>dashboard</div>
+  )
+}
+
+export default Dashboard
